502aa72c4bbd6f8da7ffd8294e16f0e11895e021
greg0rkit <gregorkit89@gmail.com>
2021-01-11 19:22:06 +0200

    add getenv() for DISCORD_GUILD

--- bot_respond.py
+++ bot_respond.py
@@ -5,6 +5,7 @@ import discord
 from discord.ext import commands
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='%', intents = intents)
