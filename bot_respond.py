854a46485186de4a2528c7787db2b3f73b699468
greg0rkit <gregorkit89@gmail.com>
2021-01-11 14:00:11 +0200

    init commit

---
+++ bot_respond.py
@@ -0,0 +1,51 @@
+# bot.py
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+
+intents = discord.Intents()
+intents.members = True
+intents.guilds = True
+bot = commands.Bot(command_prefix='%', intents = intents)
+
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        if guild.name==GUILD:
+            break
+    
+    print(
+        f'{bot.user.name} is connected to the following guild: \n'
+        f'{guild.name}\n'
+    )
+
+    print(
+        f'Amount of members: {guild.member_count}\n'
+    )
+
+@bot.command(name="zn")
+async def pinging_bot(ctx):
+    await ctx.channel.send("kariolh mh ta valeis me emas")
+
+@bot.command(name="print")
+async def print(ctx, args):
+    await ctx.channel.send(args)
+
+
+
+bot.run(TOKEN)
+
+
+def print_members(bot):
+    for member in bot.guild.members:
+            
