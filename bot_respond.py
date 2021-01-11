a68369234652cad37feb5c4818fb03484afb0797
greg0rkit <gregorkit89@gmail.com>
2021-01-11 18:49:32 +0200

    add getenv() line for DISCORD_TOKEN

--- bot_respond.py
+++ bot_respond.py
@@ -4,6 +4,8 @@ import os
 import discord
 from discord.ext import commands
 
+TOKEN = os.getenv("DISCORD_TOKEN")
+
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='%', intents = intents)
 
